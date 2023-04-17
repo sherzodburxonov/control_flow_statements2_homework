53c488aba841fb5225a58bd00d7ad29c525a64f3
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-17 12:15:15 +0500

    05

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    first=n//10000
+    second=n%10000//1000
+    third=n%1000//100
+    four=n%100//10
+    five=n%10
+    a=first
+    if a<second:
+        a=second
+    if a<third:
+        a=third
+    if a<four:
+        a=four
+    if a<five:
+        a=five
+    return a
+print(main(93546))
