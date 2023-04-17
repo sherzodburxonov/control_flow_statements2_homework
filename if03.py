6e60b2bd0118f0309c9835ae2024fbaeb5088404
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-17 12:07:27 +0500

    03

--- if03.py
+++ if03.py
@@ -8,4 +8,20 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    z=a
+    x=a
+    d=a
+    if z<b:
+        z=b
+    if z<c:
+        z=c
+    if z>b:
+        x=b
+    if z>c:
+        x=c
+    if x==d or z==d:
+        d=b
+    if x==d or z==d:
+        d=c
+    return d
+print(main(3,2,1))
